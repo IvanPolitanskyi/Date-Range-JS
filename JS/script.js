48e342e123870f3a712fd352556052a38a025a8c
Vanya Politanskiy <vanyapolitanskiy@gmail.com>
2020-08-29 18:37:59 +0300

    We display information on a period of time.

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,9 @@ document.querySelector('button').onclick = function () {
     let dateEnd = document.querySelector('#end-date').value;
     dateStart = Date.parse(dateStart);
     dateEnd = Date.parse(dateEnd);
-    for (let i = dateStart; i < dateEnd; i = i + 24 * 60 * 60 * 1000) {
-        console.log(i);
+    let out = document.querySelector('#out');
+    for (let i = dateStart; i <= dateEnd; i = i + 24 * 60 * 60 * 1000) {
+        out.innerHTML += new Date(i).toISOString().substr(0, 10) +'<br>';
     }
-    console.log(dateStart);
+    //moment.js — бибилиотека для работы с датами. 
 }
